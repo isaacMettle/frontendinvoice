fb9596833ddf33c287f63525e9f1d5b899d8695f
isaacMettle <lareisaac12@gmail.com>
2024-08-07 19:31:13 +0000

    third

---
+++ src/views/dossier/chat.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<template>
+    <h1>Hello word</h1>
+</template>
